765766edb9ae8e3222b9993843d42258cfbf0a95
Dreddsparc <sparc399@gmail.com>
2018-01-01 18:21:31 -0600

    It's a bit fancier now!
    Added a couple lists to the mix:
    greets : a list of words synonymous with 'hello'
    planet : a list of words synonymous with 'world'
    
    the output print will look at the lengths of each list and grab
    a random word to form the final greeting.

--- helloworld.py
+++ helloworld.py
@@ -3,8 +3,21 @@
 # FileName : helloworld.py
 #
 # Description : The main workhorse of this project!  The Place where all the magic Happens
+#               This program, when run will print a random greeting to the screen using synonyms
+#               for 'hello' and 'world'
+#               Variables greets and planet are lists and can have words added to allow for a wider
+#               range of available greetings
+#
+# Version : 1.01
+#
+# Author : Christian Weaver  aka: Dreddsparc
 #
 ##########################################################################################
+from random import *
+
+greets = ['Greetings', 'Hi', 'Howdy', 'Bonjour', 'Hey', 'Howdy-Do', "What's Up", 'Hello']
+planet = ['World', 'Earth', 'Nature', 'Cosmos', 'Macrocosm', 'Universe', 'Terra']
 
 if __name__ == '__main__':
-    print("Hello World!")
+
+    print("{0} {1}".format(greets[randint(1, len(greets)-1)], planet[randint(1, len(planet)-1)], end=""))
